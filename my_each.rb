1b5cf1723541f1bca68ab3e0b1ce0b3861f386f7
Tim Quimbo <tquimbo1@gmail.com>
2021-03-03 17:32:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,14 @@
-def my_each # put argument(s) here
-  collection = [1, 2, 3, 4]
-  my_each(collection) do |i|
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
